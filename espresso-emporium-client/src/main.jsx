763a9b16f07068f5f43a1f957a8eb75f8698b58e
protapkrghosh <protapg518@gmail.com>
2025-05-16 18:30:54 +0600

    setup server

--- espresso-emporium-client/src/main.jsx
+++ espresso-emporium-client/src/main.jsx
@@ -1,12 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./Layouts/MainLayout.jsx";
+import Home from "./components/Home.jsx";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      Component: MainLayout,
+      children: [
+         {
+            index: true,
+            Component: Home,
+         },
+         {
+            path: "addCoffee",
+            Component: AddCoffee,
+         },
+         {
+            path: "updateCoffee",
+            Component: UpdateCoffee,
+         },
+      ],
+   },
+]);
 
 createRoot(document.getElementById("root")).render(
    <StrictMode>
-      <App />
+      <RouterProvider router={router} />
       <Toaster />
    </StrictMode>
 );
--- espresso-emporium-server/index.js
+++ espresso-emporium-server/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrk8jch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   },
+});
+
+async function run() {
+   try {
+      await client.connect();
+
+      await client.db("admin").command({ ping: 1 });
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // await client.close();
+   }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
    res.send("Espresso Emporium server is getting hotter");
@@ -14,4 +41,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
    console.log(`Espresso Emporium server is running on port ${port}`);
 });
-
