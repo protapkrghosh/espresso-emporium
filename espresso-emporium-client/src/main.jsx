7c0af6400320f2098ae6b4daf37d43a524c6cf27
protapkrghosh <protapg518@gmail.com>
2025-08-27 23:39:38 +0600

    initial commit

---
+++ espresso-emporium-client/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <div>Hello World</div>,
+   },
+]);
+
+createRoot(document.getElementById("root")).render(
+   <StrictMode>
+      <RouterProvider router={router} />,
+      <Toaster />
+   </StrictMode>
+);
